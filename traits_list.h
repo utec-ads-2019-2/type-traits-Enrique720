b0f7b3bfe0395ae116a48d754b005499b2ed9d0d
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-17 22:45:51 -0500

    Update traits_list.h

--- traits_list.h
+++ traits_list.h
@@ -4,49 +4,96 @@
 #include "node.h"
 
 template <typename Tr>
-class TraitsList {     
-    public:
-        typedef typename Tr::T T;
-        typedef typename Tr::Operation Operation;
-      
-    private:
-        Node<T>* head;
-        Operation cmp;
+class TraitsList {
+public:
+    typedef typename Tr::T T;
+    typedef typename Tr::Operation Operation;
 
-        bool find(T data, Node<T> **&pointer) {
-            // TODO
-        }
-              
-    public:
-        TraitsList() : head(nullptr) {};
-             
-        bool insert(T data) {
-            // TODO
-        }
-             
-        bool remove(T data) {
-            // TODO
-        }  
+private:
+    Node<T>* head;
+    Operation cmp;
+    int nodes;
 
-        bool find(T data) {
-            // TODO
-        }
+    bool find(T data, Node<T> **&pointer) {
+        while((*pointer) != nullptr){
 
-        T operator [] (int index) {
-            // TODO
+            if ((*pointer)->data == data){
+                return true;
+            }
+            else if(cmp(data,(*pointer)->data)){
+                break;
+            }
+            pointer = &((*pointer)->next);
         }
-             
-        int size() {
-            // TODO
-        }
+        return false;
+    }
+public:
+    TraitsList() : head(nullptr),nodes{0} {};
 
-        void print() {
-            // TODO
+    bool insert(T data) {
+        auto ** pointer = &(this->head);
+        if(!find(data, pointer)){
+            auto* temp = new Node<T>(data);
+            temp->next = (*pointer);
+            (*pointer) = temp;
+            this->nodes++;
+            return true;
         }
+        else{
+            return false;
+        }
+    }
+    bool remove(T data) {
+         auto ** pointer = &(this->head);
+        if(find(data,pointer)){
+            auto* temp = (*pointer);
+            (*pointer) = temp->next;
 
-        ~TraitsList() {
-            // TODO
-        }         
+            delete temp;
+            (this->nodes)--;
+            return true;
+        }
+        return false;
+    }
+    bool find(T data) {
+        auto** temp = &(this->head);
+        return find(data,temp);
+
+    }
+
+    T operator [] (int index) {
+        if(index < nodes && index >= 0){
+            auto * it = head;
+            for(int i = 0; i < index ; i++){
+                it = it->next;
+            }
+            return it->data;
+        }
+        return -1;
+    }
+
+    int size() {
+        return nodes;
+    }
+
+    void print() {
+        auto * temp =this->head;
+        for(int i = 0 ;i < this->nodes; i ++){
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+
+
+    ~TraitsList() {
+        if(nodes == 0){
+            delete this;
+        }
+        else{
+            this->head->killSelf();
+            this->head = nullptr;
+            this->nodes = 0;
+        }
+    }
 };
-
 #endif
